b53ad1358f2f3127fd3cac6cf46b2369f61c42df
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-09-14 11:03:13 -0300

    Update ListSocialMediaController.js

--- src/controller/ListSocialMediaController.js
+++ src/controller/ListSocialMediaController.js
@@ -4,15 +4,15 @@ export default {
     async index(req, res){
             const urlTwitter = 'http://codefight.davidbanham.com/twitter';
             const resultTwitter = await fetch(urlTwitter);
-            let twitter = await resultTwitter.json()
+            const twitter = await resultTwitter.json()
             
             const urlFacebook = 'http://codefight.davidbanham.com/facebook';
             const resultFacebook = await fetch(urlFacebook);
-            let facebook = await resultFacebook.json()
+            const facebook = await resultFacebook.json()
 
             const urlInstagram = 'http://codefight.davidbanham.com/instagram';
             const resultInstagram = await fetch(urlInstagram);
-            let instagram = await resultInstagram.json()
+            const instagram = await resultInstagram.json()
 
             return res.send({twitter, facebook, instagram})
 
