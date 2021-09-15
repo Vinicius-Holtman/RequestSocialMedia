a5e293f2131e7147a53974d92f248b6587dff19f
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-09-15 15:07:54 -0300

    Update ListSocialMediaController.js

--- src/controller/ListSocialMediaController.js
+++ src/controller/ListSocialMediaController.js
@@ -2,19 +2,34 @@ import fetch from 'node-fetch';
 
 export default {
     async index(req, res){
-            const urlTwitter = 'http://codefight.davidbanham.com/twitter';
-            const resultTwitter = await fetch(urlTwitter);
-            const twitter = await resultTwitter.json()
-            
-            const urlFacebook = 'http://codefight.davidbanham.com/facebook';
-            const resultFacebook = await fetch(urlFacebook);
-            const facebook = await resultFacebook.json()
 
-            const urlInstagram = 'http://codefight.davidbanham.com/instagram';
-            const resultInstagram = await fetch(urlInstagram);
-            const instagram = await resultInstagram.json()
+        const twitter = await fetch('http://codefight.davidbanham.com/twitter').then(async response => {
+            try {
+                const urlTwitter = await response.json()
+                return urlTwitter
+            } catch (error) {
+                return "I am trapped in a social media factory send help"       
+            }
+        })
 
-            return res.send({twitter, facebook, instagram})
+        const facebook = await fetch('http://codefight.davidbanham.com/facebook').then(async response => {
+            try {
+                const urlFacebook = await response.json()
+                return urlFacebook
+            } catch (error) {
+                return "I am trapped in a social media factory send help"       
+            }
+        })
 
+        const instagram = await fetch('http://codefight.davidbanham.com/instagram').then(async response => {
+            try {
+                const urlInstagram = await response.json()
+                return urlInstagram
+            } catch (error) {
+                return "I am trapped in a social media factory send help"       
+            }
+        })
+
+        return res.json({ twitter, facebook, instagram })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,6 @@
 import express from 'express';
 import ListSocialMedia from './controller/ListSocialMediaController.js'
-import Twitter from './controller/TwitterController.js'
-import Facebook from './controller/FacebookController.js'
-import Instagram from './controller/InstagramController.js'
 
 export const routes = express.Router();
 
 routes.get("/", ListSocialMedia.index)
-routes.get("/twitter", Twitter.index)
-routes.get("/facebook", Facebook.index)
-routes.get("/instagram", Instagram.index)
